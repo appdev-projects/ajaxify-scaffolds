e7f83349b9108828c1ec1a461a5a8d1ff95e2845
mlemansk <mlemansk@chicagobooth.edu>
2021-05-09 21:17:45 +0000

    Ajaxing Create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies or /movies.json
   def index
     @movies = Movie.all
+    @movie = Movie.new
   end
 
   # GET /movies/1 or /movies/1.json
@@ -27,9 +28,11 @@ class MoviesController < ApplicationController
       if @movie.save
         format.html { redirect_to @movie, notice: "Movie was successfully created." }
         format.json { render :show, status: :created, location: @movie }
+        format.js { render template: "movies/create.js.erb" }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @movie.errors, status: :unprocessable_entity }
+        format.js { render template: "movies/create_error.js.erb" }
       end
     end
   end
