40724bdacb76ee9e4086bad4c600fbb2f83fda8c
Phil255 <philipp.spitta@chicagobooth.edu>
2021-05-10 01:29:32 +0000

    added jquery and bootstrap

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: directors
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  bio        :text
+#  dob        :date
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Director < ApplicationRecord
+  has_many :movies
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :text
+#  duration    :integer
+#  image       :string
+#  year        :integer
+#  director_id :bigint
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Movie < ApplicationRecord
   belongs_to :director
+  has_many :characters, dependent: :destroy
+  has_many :actors, through: :characters
 end
