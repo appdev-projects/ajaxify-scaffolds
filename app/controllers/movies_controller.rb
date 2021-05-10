047745527abb4c3ec809b881aaacb093bf626b0d
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-05-10 02:09:34 +0000

    fix form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,7 @@ class MoviesController < ApplicationController
   # GET /movies or /movies.json
   def index
     @movies = Movie.all
+    @movie = Movie.new
   end
 
   # GET /movies/1 or /movies/1.json
