31c6846f1cf64a85b6a0d1630ddc6231b0f95538
ddimitrov777 <ddimitro@chicagobooth.edu>
2021-05-11 02:25:22 +0000

    stuff

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: characters
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  actor_id   :bigint           not null
+#  movie_id   :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Character < ApplicationRecord
   belongs_to :actor
   belongs_to :movie
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: directors
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  bio        :text
+#  dob        :date
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Director < ApplicationRecord
 end
