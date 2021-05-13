c416af48ce75f33e86ce4bde712010ed639ec321
hnguyen999 <hnguyen0@chicagobooth.edu>
2021-05-13 22:16:58 +0000

    Added HTML for edit row in Movies Index

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: actors
+#
+#  id         :bigint           not null, primary key
+#  bio        :text
+#  dob        :date
+#  image      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Actor < ApplicationRecord
+  has_many(:characters, {
+    :class_name => "Character",
+    :foreign_key => "actor_id",
+    dependent: :destroy
+  })
+
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: directors
+#
+#  id         :bigint           not null, primary key
+#  bio        :text
+#  dob        :date
+#  image      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Director < ApplicationRecord
+    has_many(:movies, {
+    :class_name => "Movie",
+    :foreign_key => "director_id",
+    dependent: :destroy
+  })
+
+
 end
