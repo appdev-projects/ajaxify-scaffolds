5310d856844c4ed7e4abacd04675466928ed5d03
hnguyen999 <hnguyen0@chicagobooth.edu>
2021-05-16 01:34:58 +0000

    added bootstrap CDN and styled New Movie button

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  duration    :integer
+#  image       :string
+#  title       :string
+#  year        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  director_id :bigint
+#
+# Indexes
+#
+#  index_movies_on_director_id  (director_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (director_id => directors.id)
+#
 require "test_helper"
 
 class MovieTest < ActiveSupport::TestCase
