2c0fceca1f0338e67c2421f212a675f4819476cf
WillSchwieder <wschwied@chicagobooth.edu>
2022-05-03 23:35:18 +0000

    User can edit the entire line for movie details

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root "movies#index"
 
+  
+
+
   resources :characters
   resources :actors
   resources :directors
   resources :movies
+
+  get("/edit_year", { :controller => "movies", :action => "edit_year" })
+
+
 end
