cd9df365b0f9f64de275d9fa8b179a14a0fc6267
lepst0928 <lepstein@chicagobooth.edu>
2022-05-05 20:46:25 +0000

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -27,6 +27,7 @@ class MoviesController < ApplicationController
       if @movie.save
         format.html { redirect_to @movie, notice: "Movie was successfully created." }
         format.json { render :show, status: :created, location: @movie }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @movie.errors, status: :unprocessable_entity }
