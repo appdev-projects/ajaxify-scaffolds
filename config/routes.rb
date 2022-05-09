d89fbe7c31929f8cf06e1d8b1ae3394aa5fcbc4f
claycurran0 <ccurran@chicagobooth.edu>
2022-05-09 19:29:24 +0000

    built ajax for edit movie title

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,28 @@
 Rails.application.routes.draw do
   root "movies#index"
 
-  resources :characters
-  resources :actors
-  resources :directors
-  resources :movies
+  resources :characters do
+    get "actor_id", as: :edit_actor_id
+    get "movie_id", as: :edit_movie_id
+  end
+
+  resources :actors do
+    get "name/edit", as: :edit_name
+    get "bio/edit", as: :edit_bio
+    get "dob/edit", as: :edit_dob
+  end
+
+  resources :directors do
+    get "name/edit", as: :edit_name
+    get "bio/edit", as: :edit_bio
+    get "dob/edit", as: :edit_dob
+  end
+
+  resources :movies do
+    get "title/edit" => "movies#edit_title", as: :edit_title
+    get "description/edit" => "movies#edit_description", as: :edit_description
+    get "duration/edit" => "movies#edit_duration", as: :edit_duration
+    get "year/edit" => "movies#edit_year", as: :edit_year
+    get "director_id/edit" => "movies#edit_director_id", as: :edit_director_id
+  end
 end
